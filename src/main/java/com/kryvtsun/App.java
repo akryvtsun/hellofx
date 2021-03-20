7cbc432fdbce0e53bbc33a1e56b4b56d4742aa23
akryvtsun <kontiky@gmail.com>
2021-03-20 22:05:03 +0200

    add center button

--- src/main/java/com/kryvtsun/App.java
+++ src/main/java/com/kryvtsun/App.java
@@ -2,20 +2,33 @@ package com.kryvtsun;
 
 import javafx.application.Application;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
+import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
 public class App extends Application {
 
     @Override
-    public void start(Stage stage) {
-        String javaVersion = System.getProperty("java.version");
-        String javafxVersion = System.getProperty("javafx.version");
-        Label l = new Label("Hello, JavaFX " + javafxVersion + ", running on Java " + javaVersion + ".");
-        Scene scene = new Scene(new StackPane(l), 640, 480);
-        stage.setScene(scene);
-        stage.show();
+    public void start(Stage s) {
+        // set title for the stage
+        s.setTitle("creating buttons");
+
+        // create a button
+        Button b = new Button("button");
+
+        // create a stack pane
+        StackPane r = new StackPane();
+
+        // add button
+        r.getChildren().add(b);
+
+        // create a scene
+        Scene sc = new Scene(r, 640, 480);
+
+        // set the scene
+        s.setScene(sc);
+
+        s.show();
     }
 
     public static void main(String[] args) {
